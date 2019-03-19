fe2583fcac68991b60a28bdc03251bdac5f34c4b
Mahlatse Motsamai <mpmotsamai4@gmail.com>
2019-03-19 15:48:33 +0200

    First commit

---
+++ recursion.py
@@ -0,0 +1,18 @@
+def sum_array(array):
+
+    '''Return sum of all items in array'''
+    def fibonacci(n):
+
+    '''Return nth term in fibonacci sequence'''
+
+    def factorial(n):
+def factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n * factorial(n-1)
+    '''Return n!'''
+
+    def reverse(word):
+
+    '''Return word in reverse'''
