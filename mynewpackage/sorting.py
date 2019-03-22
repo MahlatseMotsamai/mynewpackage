4a6daf9a92f48e51a2e61953e21d57b77d992327
Mahlatse Motsamai <mpmotsamai4@gmail.com>
2019-03-22 11:48:37 +0200

    new changes

--- mynewpackage/sorting.py
+++ mynewpackage/sorting.py
@@ -12,36 +12,31 @@ def bubble_sort(items):
 # Merge sort returns a sorted list.
 def merge_sort(items):
 
-    len_i = len(items)
-    # Base case. A list of size 1 is sorted.
-    # Cae returns, so if reached then function will terminate after line 8
-    if len_i == 1:
+   def merge(A, B):
+     new_list = []
+     while len(A) > 0 and len(B) > 0:
+        if A[0] < B[0]:
+            new_list.append(A[0])
+            A.pop(0)
+        else:
+            new_list.append(B[0])
+            B.pop(0)
+      if len(A) == 0:
+          new_list = new_list + B
+      elif len(B) == 0:
+          new_list = new_list + A
+
+
+      return new_list
+
+   len_i == len(items)
+   if len_i == 1
         return items
-    # Recursive case. Find midpoint of list
+
     mid_point = int(len_i / 2)
-    # divide list into two halves
     i1 = merge_sort(items[:mid_point])
     i2 = merge_sort(items[mid_point:])
-    # call merge_sort function on each half of list
 
-
-# Merge function.
-    def merge(A, B):
-        new_list = []
-        while len(A) > 0 and len(B) > 0:
-            if A[0] < B[0]:
-                new_list.append(A[0])
-                A.pop(0)
-            else:
-                new_list.append(B[0])
-                B.pop(0)
-
-        if len(A) == 0:
-            new_list = new_list + B
-        elif len(B) == 0:
-            new_list = new_list + A
-
-        return new_list
 
     return merge(i1, i2)
 
