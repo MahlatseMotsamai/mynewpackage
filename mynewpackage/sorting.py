5ead59d203949f93462a621e84e39bf2a5f017a5
Mahlatse Motsamai <mpmotsamai4@gmail.com>
2019-03-22 11:23:08 +0200

    make changes to sorting

--- mynewpackage/sorting.py
+++ mynewpackage/sorting.py
@@ -9,19 +9,40 @@ def bubble_sort(items):
     return items
 
 
+# Merge sort returns a sorted list.
 def merge_sort(items):
 
-    '''Return array of items, sorted in ascending order'''
-    def lin_merge(list1, list2):
+    len_i = len(items)
+    # Base case. A list of size 1 is sorted.
+    # Cae returns, so if reached then function will terminate after line 8
+    if len_i == 1:
+        return items
+    # Recursive case. Find midpoint of list
+    mid_point = int(len_i / 2)
+    # divide list into two halves
+    i1 = merge_sort(items[:mid_point])
+    i2 = merge_sort(items[mid_point:])
+    # call merge_sort function on each half of list
+    return merge(i1, i2)
 
-        new_list = []
-        while (len(list1) > 0  and len(list2) > 0):
-            if list1[0] < list2[0]:
-                new_list.append(list1.pop(0))
-            else:
-                new_list.append(list2.pop(0))
+# Merge function.
+def merge(A, B):
+    new_list = []
+    while len(A) > 0 and len(B) > 0:
+        if A[0] < B[0]:
+            new_list.append(A[0])
+            A.pop(0)
+        else:
+            new_list.append(B[0])
+            B.pop(0)
 
-        return new_list + list1 + list2
+    if len(A) == 0:
+        new_list = new_list + B
+    if len(B) == 0:
+        new_list = new_list + A
+
+    return new_list
+
 
 def quick_sort(items):
 
