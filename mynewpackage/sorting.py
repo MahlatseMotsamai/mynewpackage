2d95019e30e47bb33ba371fc97fb611dfd16c0fc
Mahlatse Motsamai <mpmotsamai4@gmail.com>
2019-03-22 11:28:07 +0200

    another changes

--- mynewpackage/sorting.py
+++ mynewpackage/sorting.py
@@ -23,26 +23,27 @@ def merge_sort(items):
     i1 = merge_sort(items[:mid_point])
     i2 = merge_sort(items[mid_point:])
     # call merge_sort function on each half of list
-    return merge(i1, i2)
+
 
 # Merge function.
-def merge(A, B):
-    new_list = []
-    while len(A) > 0 and len(B) > 0:
-        if A[0] < B[0]:
-            new_list.append(A[0])
-            A.pop(0)
-        else:
-            new_list.append(B[0])
-            B.pop(0)
+    def merge(A, B):
+        new_list = []
+        while len(A) > 0 and len(B) > 0:
+            if A[0] < B[0]:
+                new_list.append(A[0])
+                A.pop(0)
+            else:
+                new_list.append(B[0])
+                B.pop(0)
 
-    if len(A) == 0:
-        new_list = new_list + B
-    if len(B) == 0:
-        new_list = new_list + A
+        if len(A) == 0:
+            new_list = new_list + B
+        if len(B) == 0:
+            new_list = new_list + A
 
-    return new_list
-
+        return new_list
+        
+    return merge(i1, i2)
 
 def quick_sort(items):
 
